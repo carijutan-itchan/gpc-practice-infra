b6addaea489ef84c93e9d26147fc9d5a770e2a01
carijutan-itchan <carijutan.christian@gmail.com>
2026-02-09 15:19:47 +0800

    Add VPC network, public and private subnet configurations, and SSH firewall rule

--- network.tf
+++ network.tf
@@ -2,3 +2,31 @@ resource "google_compute_network" "vpc_network" {
   name = "vpc-network"
   auto_create_subnetworks = false
 }
+
+
+resource "google_compute_subnetwork" "public_subnet" {
+  name          = "public-subnet"
+  ip_cidr_range = "10.0.1.0/24"
+  region        = "us-central1"
+  network       = google_compute_network.vpc_network.id
+}
+
+resource "google_compute_subnetwork" "private_subnet" {
+  name          = "private-subnet"
+  ip_cidr_range = "10.0.2.0/24"
+  region        = "us-central1"
+  network       = google_compute_network.vpc_network.id
+}
+
+resource "google_compute_firewall" "allow_ssh" {
+  name    = "allow-ssh"
+  network = google_compute_network.vpc_network.id
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["bastion"]
+}
